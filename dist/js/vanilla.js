a161df5c8e588b284981f0dc35d867fb1168163f
Paweł <kontakt@pawelcybulski.pl>
2018-12-02 00:16:06 +0100

    first commit

---
+++ dist/js/vanilla.js
@@ -0,0 +1,47 @@
+header = {};
+header.sentences = []
+overview = {};
+keynoteSpeach = {};
+
+
+// Main InitView Function 
+window.onload = function () {
+    // Pass here all functions which should start at the beginning 
+}
+
+// HEADER
+
+
+
+// OVERVIEW
+
+
+
+// ANOTHER SECTION
+
+
+
+
+
+
+
+// Long arrays and HTML templates leave at the bottom of the file
+
+header.sentences = [{
+        country: 'poland',
+        year: '51 LAT (1967-2018)',
+        sentence: 'GDAŃSKIEJ INFORMATYKI EKONOMICZNEJ'
+    },
+    {
+        country: 'england',
+        year: '51 YEARS (1967-2018) OF',
+        sentence: 'BUSINESS INFORMATICS AT UNIVERSITY OF GDANSK'
+    },
+    {
+        country: 'bulgari',
+        year: '51 ГОДИНИ (1967-2018)',
+        sentence: 'НА БИЗНЕС ИНФОРМАТИКАВ УНИВЕРСИТЕТА НА ГДАНСК'
+    }
+
+
+]
---
+++ readme.txt
@@ -0,0 +1,11 @@
+# GIT
+[new], [update], [hotfix]
+git pull - before start working
+Use .gitignore if you need
+
+# CSS
+Use BEM methodology
+
+# JS
+Use objects for all sections
+
