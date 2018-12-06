3c1511607d71cfe63614a4670281b8854c17357e
Paweł <kontakt@pawelcybulski.pl>
2018-12-06 23:03:25 +0100

    [new] keynote speach & relevant topics & footer

--- dist/js/vanilla.js
+++ dist/js/vanilla.js
@@ -2,19 +2,21 @@ header = {};
 header.sentences = [];
 overview = {};
 keynoteSpeach = {};
+releventTopics = {};
 
 // Main InitView Function
-window.onload = function() {
+window.onload = function () {
   // Pass here all functions which should start at the beginning
   header.initHeaderView();
+  releventTopics.initReleventTopicView();
 };
 
 // ------------------- HEADER -------------------
-header.initHeaderView = function() {
+header.initHeaderView = function () {
   header.getSentenceContent();
 };
 
-header.setSentence = function(year, text) {
+header.setSentence = function (year, text) {
   var sentenceYear = document.querySelector(
     ".welcome-wrapper--sentence-content-year"
   );
@@ -22,16 +24,16 @@ header.setSentence = function(year, text) {
     ".welcome-wrapper--sentence-content-text"
   );
 
-  sentenceYear.textContent = year;
-  sentenceText.textContent = text;
+  // sentenceYear.textContent = year;
+  // sentenceText.textContent = text;
 };
 
-header.getSentenceContent = function() {
+header.getSentenceContent = function () {
   var sentences = header.sentences;
   var arrYear;
   var arrText;
 
-  sentences.forEach(function(sentence) {
+  sentences.forEach(function (sentence) {
     arrYear = sentence.year;
     arrText = sentence.sentence;
     header.setSentence(arrYear, arrText);
@@ -45,11 +47,41 @@ header.getSentenceContent = function() {
 
 // ------------------- OVERVIEW -------------------
 
-// ------------------- ANOTHER SECTION -------------------
+// ------------------- RELEVANT TOPICS -------------------
+releventTopics.initReleventTopicView = function () {
+  var table = document.querySelector('[data-function="topics-wrapper"]');
+
+  var topicArr = releventTopics.topicArr;
+  var index = 0;
+
+
+  while (index < topicArr.length) {
+    var tr = document.createElement('tr');
+
+    for (var i = 1; i < 4; i++) {
+      var td = document.createElement('td');
+      var cell = document.createTextNode(topicArr[index]);
+      console.log(cell);
+      if (cell == "undefined") {
+        td.appendChild("");
+      } else {
+        td.appendChild(cell);
+      }
+
+      td.appendChild(cell)
+      tr.appendChild(td);
+      index++;
+    }
+
+    table.appendChild(tr);
+  }
+
+
+};
+
 
 // Long arrays and HTML templates leave at the bottom of the file
-header.sentences = [
-  {
+header.sentences = [{
     country: "poland",
     year: "51 LAT (1967-2018)",
     sentence: "GDAŃSKIEJ INFORMATYKI EKONOMICZNEJ"
@@ -101,8 +133,7 @@ header.sentences = [
   },
   {
     country: "israel",
-    year:
-      "יובל הזהב 51 שנים (1967 - 2018) של אינפורמטיקה עסקית באוניברסיטת גדנסק, פולין",
+    year: "יובל הזהב 51 שנים (1967 - 2018) של אינפורמטיקה עסקית באוניברסיטת גדנסק, פולין",
     sentence: ""
   },
   {
@@ -133,8 +164,7 @@ header.sentences = [
   {
     country: "romania",
     year: "51 DE ANI (1967-2018)",
-    sentence:
-      "A SPECIALIZARII INFORMATICA ECONOMICA LA UNIVERSITATEA DIN GDANSK"
+    sentence: "A SPECIALIZARII INFORMATICA ECONOMICA LA UNIVERSITATEA DIN GDANSK"
   },
   {
     country: "romania",
@@ -171,4 +201,53 @@ header.sentences = [
     year: "51 РОКІВ (1967-2018)",
     sentence: "БІЗНЕС-ІНФОРМАТИКА УНІВЕРСИТЕТ ГДАНСЬКА, ПОЛЬЩА"
   }
+];
+
+releventTopics.topicArr = [
+  "Agile Methods",
+  "Big Data, Business Analytics",
+  "Blockchain Technology and Applications",
+  "Business Informatics",
+  "Business Process Modeling",
+  "Case Studies in SAND",
+  "Cloud Computing",
+  "Cognitive Issues in SAND",
+  "Conceptual Modeling",
+  "Crowdsourcing and Crowdfunding Models",
+  "Design Theory",
+  "Digital Services and Social Media",
+  "Enterprise Architecture",
+  "Enterprise Social Networks",
+  "ERP and CRM Systems",
+  "Ethical and Human Aspects of IS Development",
+  "Ethnographic, Anthropological, Action and Entrepreneurial Research",
+  "Evolution of IS Discipline",
+  "Human-Computer Interaction",
+  "Industry 4.0",
+  "Information Systems Development: Methodologies, Methods, Techniques and Tools",
+  "Internet of Things",
+  "Machine Learning",
+  "Model-Driven Architecture",
+  "New Paradigms, Formalisms, Approaches, Frameworks and Challenges in IS & SAND",
+  "Ontological Foundations and Intelligent Systems of SAND",
+  "Open Source Software (OSS) Solutions",
+  "Project Management",
+  "Quality Assurance in Systems Development",
+  "Requirements and Software Engineering",
+  "Research Methodologies in SAND",
+  "Role of SAND in Mobile and Internet Applications Development",
+  "SAND Education: Curricula, E-learning, MOOCs and Teaching Cases",
+  "SCRUM Approach",
+  "Security and Privacy Issues in IS & SAND",
+  "Service Oriented Systems Development",
+  "Social Networking Services",
+  "Socio-Technical Approaches to System Development, Psychological and Behavioural Descriptions",
+  "Software Intensive Systems and Services",
+  "Strategic Information Systems in Enterprises",
+  "Supply Chain Management Aspects",
+  "Systems Analysts and Designers Professions",
+  "Teams and Teamwork in IS & SAND",
+  "UML, SysML, BPMN",
+  "User Experience (UX) Design",
+  "Workflow Management"
 ];
