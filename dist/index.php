d3f8478f52a3b7477ec40003cb89d2cb0d1f695c
Pawel Cybulski <cybulskipaw@gmail.com>
2019-03-13 15:27:07 +0100

    [new] routing added

---
+++ dist/index.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+@ini_set('display_errors', 0);
+
+$request = $_SERVER['REDIRECT_URL'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.html';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.html';
+        break;
+    case '/registration' :
+        require __DIR__ . '/views/registration.php';
+        break;
+    default:
+        require __DIR__ . '/views/index.html';
+        break;
+}
